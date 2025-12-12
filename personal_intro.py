f95e9970e095847ef12129000a1e9c5e61f45934
mrunmayiam <118504175+mrunmayiam@users.noreply.github.com>
2025-12-12 12:24:23 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,28 @@
+print("Hello World")
+
+
+#User input
+print("Personal Information...!!!")
+
+
+#User name
+user_fname = input("What is your first name? ")
+user_lname = input("What is your last name? ")
+##User age
+user_age = input("How old are you? ")
+
+##User hobby
+user_hobby = input("What is your favourite hobby? ")
+
+##User city
+user_city = input("Which city do you live in? ")
+
+##formating using numbers
+print(f"🎉 Welcome "+user_fname.capitalize()+" "+user_lname.capitalize()+ "!  🎉")
+print(f"You are "+user_age+" years old and love "+user_hobby.capitalize()+".")
+print("You live in "+user_city.upper()+".")
+
+
+
+
+
